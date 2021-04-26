2c0602ecc755103eb70430166120c548768451c3
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-04-26 01:20:11 -0300

    Refactoring Sync

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { User } from "./models/User";
 
 const user = new User({ name: "NEW2123AME", age: 213139 });
-user.save();
+user.events.on("change", () => {
+  console.log("change");
+});
+
+user.events.trigger("change");
