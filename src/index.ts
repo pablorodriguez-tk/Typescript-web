7dd6a034ce4451c734160c5828354c6c4d805814
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-05-15 21:13:00 -0300

    Add collectionView

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-import { User } from './models/User';
-import { UserForm } from './views/UserForm';
+import { Collection } from './models/Collection';
+import { UserList } from './views/UserList';
+import { UserProps, User } from './models/User';
 
-const user = User.buildUser({ name: 'NAME', age: 20 });
-
-const root = document.querySelector('#root');
-
-if (root) {
-  const userForm = new UserForm(root, user);
-
-  userForm.render();
-} else {
-  throw new Error('Root element not found');
-}
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+users.on('change', () => {
+  const root = document.querySelector('#root');
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+users.fetch();
